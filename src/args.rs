212d4f92d45c492ab480f8f3d937c130ceeb04e3
Suyash Singh <suyash.x@gmail.com>
2023-12-10 18:44:08 +0530

    add pacman and file clap arguments

---
+++ src/args.rs
@@ -0,0 +1,116 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(
+    author = "Suyash Singh",
+    version,
+    about = "Utility to backup and restore configuration files and packages to be installed"
+)]
+pub struct ArchBakArgs {
+    #[clap(subcommand)]
+    pub entity: Entity,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Entity {
+    Pacman(PacmanCommand),
+    Files(FilesCommand),
+}
+
+#[derive(Args, Debug)]
+pub struct PacmanCommand {
+    #[clap(subcommand)]
+    pub options: PacmanCommandOptions,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum PacmanCommandOptions {
+    Backup(PackageBackupCommand),
+    Show(PackageShowCommand),
+    Restore(PackageRestoreCommand),
+}
+
+#[derive(Args, Debug)]
+pub struct PackageBackupCommand {
+    #[clap(subcommand)]
+    pub options: PackageBackup,
+}
+
+#[derive(Args, Debug)]
+pub struct PackageShowCommand {
+    #[clap(subcommand)]
+    pub options: PackageShow,
+}
+
+#[derive(Args, Debug)]
+pub struct PackageRestoreCommand {
+    #[clap(subcommand)]
+    pub options: PackageRestore,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum PackageBackup {
+    Explicit,
+    Foreign,
+    All,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum PackageShow {
+    Explicit,
+    Foreign,
+    All,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum PackageRestore {
+    Explicit,
+    Foreign,
+    All,
+}
+
+#[derive(Args, Debug)]
+pub struct FilesCommand {
+    #[clap(subcommand)]
+    pub options: FileCommandOptions,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum FileCommandOptions {
+    Add(FileAddCommand),
+    Remove(FileRemoveCommand),
+    Backup(FileBackupCommand),
+    Show(FileShowCommand),
+    ShowAll(FileShowAllCommand),
+    Restore(FileRestoreCommand),
+    RestoreAll(FileRestoreAllCommand),
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct FileAddCommand {
+    pub file: String,
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct FileRemoveCommand {
+    pub file: String,
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct FileBackupCommand {}
+
+#[derive(Args, Debug, Clone)]
+pub struct FileShowCommand {
+    pub file: String,
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct FileShowAllCommand {}
+
+#[derive(Args, Debug, Clone)]
+pub struct FileRestoreCommand {
+    pub file: String,
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct FileRestoreAllCommand {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+use args::ArchBakArgs;
+use clap::Parser;
+
+mod args;
+use args::*;
 fn main() {
-    println!("Hello, world!");
+    let args = ArchBakArgs::parse();
+    match args.entity {
+        Entity::Pacman(pacman_cmd) => match pacman_cmd.options {
+            PacmanCommandOptions::Backup(opts) => match opts {
+                PackageBackupCommand { options } => match options {
+                    PackageBackup::Explicit => {
+                        println!(" explicit backup")
+                    }
+                    PackageBackup::Foreign => {
+                        println!(" foreign backup ")
+                    }
+                    PackageBackup::All => {
+                        println!(" all backup")
+                    }
+                },
+            },
+            PacmanCommandOptions::Restore(opts) => match opts {
+                PackageRestoreCommand { options } => match options {
+                    PackageRestore::Explicit => {
+                        println!("explicit restore")
+                    }
+                    PackageRestore::Foreign => {
+                        println!("foregin restore")
+                    }
+                    PackageRestore::All => {
+                        println!("all restore")
+                    }
+                },
+            },
+            PacmanCommandOptions::Show(opts) => match opts {
+                PackageShowCommand { options } => match options {
+                    PackageShow::Explicit => {
+                        println!("explicit show")
+                    }
+                    PackageShow::Foreign => {
+                        println!("foregin show")
+                    }
+                    PackageShow::All => {
+                        println!("show all packages ")
+                    }
+                },
+            },
+        },
+        Entity::Files(files_cmd) => match files_cmd.options {
+            FileCommandOptions::Add(FileAddCommand { file }) => {
+                println!("{:?} add file", file)
+            }
+            FileCommandOptions::Remove(FileRemoveCommand { file }) => {
+                println!("{:?} remove file", file)
+            }
+            FileCommandOptions::Backup(_) => println!("backup files"),
+            FileCommandOptions::Show(FileShowCommand { file }) => {
+                println!("{:?} show file", file)
+            }
+            FileCommandOptions::ShowAll(_) => println!("show all files"),
+            FileCommandOptions::RestoreAll(_) => {
+                println!("restore all files")
+            }
+            FileCommandOptions::Restore(FileRestoreCommand { file }) => {
+                println!("{:?} restore file", file)
+            }
+        },
+    }
 }
